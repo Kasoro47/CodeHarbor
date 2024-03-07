9f477e52805757f83993d09e024f5c35d0da1894
Kasoro47 <kilian.rosak@ynov.com>
2024-03-07 11:57:28 +0100

    feat: Testing Pipeline

---
+++ Terraform-Harbor/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "kubernetes_deployment" "code-harbor" {
+  metadata {
+    name = "code-harbor"
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "code-harbor"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "code-harbor"
+        }
+      }
+      spec {
+        container {
+          image = "ghcr.io/kasoro47/my-nginx:v1"
+          name  = "code-harbor"
+        }
+        image_pull_secrets {
+          name = "ghcr-credentials"
+        }
+      }
+    }
+  }
+}
+
