4f9103244559efeedbbb50e31b32b454fed6f4b7
kilianrosak <kilian.rosak@ynov.com>
2024-07-23 10:22:09 +0200

    fix: redacting

--- scripts/codeharbor-kind-deploy.sh
+++ scripts/codeharbor-kind-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-scp k8s/kind-config.yaml k8s/codeharbor.yaml debian@57.128.61.186:~
+scp k8s/kind-config.yaml k8s/codeharbor.yaml debian@instance-ip:~
 
 # Define relative paths
 KIND_CONFIG="/home/debian/kind-config.yaml"
@@ -10,17 +10,17 @@ CLUSTER_NAME="codeharbor"
 
 # Creating kind cluster
 echo "Creating the Kind cluster..."
-ssh debian@57.128.61.186 "kind create cluster --name $CLUSTER_NAME --config=$KIND_CONFIG"
+ssh debian@instance-ip "kind create cluster --name $CLUSTER_NAME --config=$KIND_CONFIG"
 
 # Check if the Kind cluster was created and set as the current context
 echo "Checking if kubectl is configured to use the Kind cluster..."
-ssh debian@57.128.61.186 "kubectl cluster-info --context kind-$CLUSTER_NAME"
+ssh debian@instance-ip "kubectl cluster-info --context kind-$CLUSTER_NAME"
 
 if [ $? -eq 0 ]; then
     echo "Kind cluster is ready. Deploying application..."
     
     # Applying deployment file
-    ssh debian@57.128.61.186 "kubectl apply -f $DEPLOYMENT_FILE"
+    ssh debian@instance-ip "kubectl apply -f $DEPLOYMENT_FILE"
     
     echo "Application deployed successfully."
 else
